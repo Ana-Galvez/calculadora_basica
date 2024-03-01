f2094beb9c6d9a84e1e70ebe367588e3b265f4e0
Ana-Galvez <anamariagalvez01@gmail.com>
2024-03-01 15:17:28 -0300

    agregado js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function agregarAlInput(value){
-    document.getElementById('display').value += value;
+function agregarAlInput(valor) {
+  document.getElementById("display").value += valor;
+}
+
+function limpiarInput() {
+  document.getElementById("display").value = "";
+}
+
+function resultado() {
+  const result = eval(document.getElementById("display").value);
+  try {
+    document.getElementById("display").value = result;
+  } catch (error) {
+    alert('Verifique cuando pone los números y el punto')
+  }
 }
